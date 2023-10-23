a073d693f46e8cc227bf87d43047a7e1544a6679
NabilaParamitaHapsari <2118076@scholar.itn.ac.id>
2023-10-23 12:07:19 +0700

    [Bab3] State dan Props

---
+++ tugas3/data.js
@@ -0,0 +1,105 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Popular',
+    },
+    {
+      id: 2,
+      categoryName: 'Info',
+    },
+    {
+      id: 3,
+      categoryName: 'Daftar',
+    },
+    {
+      id: 4,
+      categoryName: 'Penjualan',
+    },
+    {
+      id: 5,
+      categoryName: 'Keterangan',
+    },
+  ];
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Seni Rupa Tiga Dimensi yang banyak digemari',
+      category: 'Popular',
+      image:
+        'https://imgx.sonora.id/crop/0x27:547x391/360x240/photo/2023/06/06/screenshot-2023-06-06-111704jpg-20230606111718.jpg',
+      createdAt: '23-10-2023',
+      totalComments: 89,
+    },
+    {
+      id: 2,
+      title: 'Seni Rupa Patung',
+      category: 'Ragam',
+      image:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1623398489/aohysqitqmyhaa8nmkxg.jpg',
+      createdAt: '23-10-2023',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Seni Rupa Guci dari Tanah Liat',
+      category: 'Ragam',
+      image:
+        'https://www.daun.id/cdn/999/1/HLBdToPblTdXU2tnKz9JDpwkgiPVFPWZHF6ToNBr.jpg',
+      createdAt: '23-10-2023',
+      totalComments: 89,
+    },
+    {
+      id: 4,
+      title: 'Seni Rupa Modern ',
+      category: 'Daftar',
+      image:
+        'https://bahassemua.com/wp-content/uploads/2020/10/Seni-Rupa-3-Dimensi-1.jpg',
+      createdAt: '23-10-2023',
+      totalComments: 89,
+    },
+    {
+      id: 5,
+      title: 'Seni Rupa Tiga Dimensi berbahan baku keramik',
+      category: 'Info',
+      image:
+        'https://blog-static.mamikos.com/wp-content/uploads/2022/10/Contoh-karya-seni-rupa-3-dimensi_2.jpg',
+      // createdAt: '23-10-2023',
+      // totalComments: 89,
+    },
+    {
+      id: 6,
+      title: 'Apa Itu Seni Rupa',
+      category: 'Info',
+      image:
+        'https://balitteknologikaret.co.id/wp-content/uploads/2023/04/Apa-Itu-Seni-Rupa-Tiga-Dimensi.jpg',
+    //   createdAt: 'Nov 10, 2023',
+    //   totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Seni Rupa Tiga DImensi',
+      category: 'Penjelasan',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxqYfI3QaLJVErTx3mzEWKtJq6tE8-q6o_jQ&usqp=CAU',
+    //   createdAt: 'Nov 10, 2023',
+    //   totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Macam-Macam Seni Rupa Tiga Dimensi',
+      category: 'Daftar',
+      image:
+        'https://berita.99.co/wp-content/uploads/2021/11/topeng.jpg',
+    //   createdAt: 'Nov 10, 2023',
+    //   totalComments: 89,
+    },
+    {
+      id: 9,
+      title: ' List Penjualan',
+      category: 'Penjualan',
+      image:
+        'https://imgx.sonora.id/crop/0x0:0x0/x/photo/2022/12/07/patung-pixabay-raplhjpg-20221207103136.jpg',
+    //   createdAt: 'Nov 10, 2023',
+    //   totalComments: 89,
+    },
+  ];
